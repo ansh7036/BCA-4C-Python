e51f3bc3b872d9841fbc98cd6fda76a6d857f833
ansh7036 <161555574+ansh7036@users.noreply.github.com>
2024-04-23 20:23:59 +0530

    main.py

--- python/main.py
+++ python/main.py
@@ -1,5 +1,5 @@
 #main.py
 from prime import prime_function
-print("Name: Dhruv\nRoll No: 2210997071")
+print("Name: Anah\nRoll No: 2210997036")
 a = int(input("Enter a number: "))
 prime_function(a)
